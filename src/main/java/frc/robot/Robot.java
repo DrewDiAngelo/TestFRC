53e0d28bdebe6fd315230069da24228155040ebd
Andrew DiAngelo <DiAngelo.A@parmacityschools.org>
2019-01-14 18:23:33 -0500

    Rewrote Code
    The code for the lift here was based off of last years lift design.
    This required new software, including new codes and other tech words.
    The code has a few bugs that can be worked out tomorrow.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -32,6 +32,8 @@ public class Robot extends TimedRobot {
 
 /*Not a good start*/
 
+/* Cannot do Endcoder value change due to not shuffleboard */
+
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
